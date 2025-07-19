f31585f198741a93621b43564e70fa4204724be2
Jahan <you@example.com>
2025-07-20 01:04:43 +0330

    comment section

--- script/script.js
+++ script/script.js
@@ -7,3 +7,36 @@ menuOpenBtn.addEventListener('click',event=>{
 })
 
 menuCloseBtn.addEventListener("click",()=>menuOpenBtn.click())
+
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween:25,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // responsive Breakpoints
+    breakpoints : {
+        0: {
+            slidesPerView: 1
+        },
+        768 : {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    }
+    
+  });
